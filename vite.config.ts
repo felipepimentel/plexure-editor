b26f8ac9a32d8efed0b6110b65ea9f0a1afb7256
Felipe Pimentel <fpimentel88@gmail.com>
2024-12-21 16:25:37 -0300

    Update dependencies, enhance TypeScript configuration, and refine Vite setup: Added js-yaml and updated various dependencies including React and ESLint packages for improved performance and stability. Modified TypeScript configuration files to streamline project structure and enhance module resolution. Adjusted Vite configuration to optimize dependency handling. These changes aim to improve overall application maintainability and user experience.

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,15 @@
-/// <reference types="vite/client" />
-
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    },
-  },
-  build: {
-    target: 'esnext',
-    sourcemap: false, // Desativa a geração de source maps
-  },
-  optimizeDeps: {
-    exclude: [], // Remova 'monaco-editor' e 'monaco-yaml' da exclusão
   },
 });
